df167d3575c95f03cb5292c36aec4a4997f11636
Hassankhaleel <hk450408@gmail.com>
2025-09-07 01:39:47 +0500

    setup completed:

---
+++ gym-system/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import "@radix-ui/themes/styles.css";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+function App() {
+
+  return (
+    <>
+
+
+
+      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="edit">
+          <EditIcon />
+        </Fab>
+        <Fab variant="extended">
+          <NavigationIcon sx={{ mr: 1 }} />
+          Navigate
+        </Fab>
+        <Fab disabled aria-label="like">
+          <FavoriteIcon />
+        </Fab>
+      </Box>
+
+
+    </>
+  )
+}
+
+export default App
